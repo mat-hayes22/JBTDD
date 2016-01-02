2c620170fb65233bd8d506e42ac0292004c08095
mat hayes <mat.hayes@outlook.com>
2016-01-02 18:44:46 +0000

    no longer fake denominator

--- production/source/mh/maths/Fraction/Fraction.java
+++ production/source/mh/maths/Fraction/Fraction.java
@@ -6,20 +6,23 @@ package mh.maths.Fraction;
 public class Fraction {
 
 
-    private int integerValue;
+    private final int denominator;
+    private final int integerValue;
 
     public Fraction(int integerValue) {
 
         this.integerValue = integerValue;
+        this.denominator = 1;
     }
 
     public Fraction(int numerator, int denominator) {
-        
+        this.integerValue = numerator;
+        this.denominator = denominator;
     }
 
     public Fraction plus(Fraction that) {
 
-            return new Fraction(this.integerValue + that.integerValue );
+            return new Fraction(this.integerValue + that.integerValue, denominator );
     }
 
     public int intValue() {
