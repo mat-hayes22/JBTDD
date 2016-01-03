648fad817b0af8376a07ac84c49240f6bbc5f8ae
mat hayes <mat.hayes@outlook.com>
2016-01-03 11:16:13 +0000

    tested GCD - found it comes out negative... dont want this. changed it to abs.

--- test/source/mh/maths/test/GreatestCommonDivisorTest.java
+++ test/source/mh/maths/test/GreatestCommonDivisorTest.java
@@ -13,14 +13,14 @@ public class GreatestCommonDivisorTest {
     public void reflexive() throws Exception {
         assertEquals(1, gcd(1,1));
         assertEquals(2, gcd(2,2));
-        assertEquals(-2, gcd(-2,-2));
+        assertEquals(2, gcd(-2,-2));
     }
 
     @Test
     public void relativelyPrime() throws Exception {
         assertEquals(1, gcd(2,3));
         assertEquals(1, gcd(4,7));
-        assertEquals(-1, gcd(-4,-7));
+        assertEquals(1, gcd(-4,-7));
     }
     @Test
     public void multipleOfOther() throws Exception {
@@ -32,7 +32,7 @@ public class GreatestCommonDivisorTest {
     public void commonFactor() throws Exception {
         assertEquals(2, gcd(6,8));
         assertEquals(7, gcd(49,315));
-        assertEquals(-4, gcd(-24,-28));
+        assertEquals(4, gcd(-24,-28));
     }
 
 
@@ -43,7 +43,7 @@ public class GreatestCommonDivisorTest {
             b = a%t;
             a =t;
         }
-        return a;
+        return Math.abs(a);
     }
 }
 
