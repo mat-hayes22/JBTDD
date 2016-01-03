618f83357c2eaad738bc78e80a1b612b8ef1c077
mat hayes <mat.hayes@outlook.com>
2016-01-03 09:46:13 +0000

    implemented euqals tests

--- test/source/mh/maths/test/FractionEqualsTest.java
+++ test/source/mh/maths/test/FractionEqualsTest.java
@@ -4,6 +4,7 @@ import mh.maths.Fraction.Fraction;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 /**
  * Created by mat on 02/01/2016.
@@ -12,9 +13,35 @@ public class FractionEqualsTest {
 
     @Test
     public void sameNumeratorAndDenominator() throws Exception {
-
-
         assertEquals(new Fraction(3,5), new Fraction(3,5));
     }
+
+    @Test
+    public void differentNumerators() throws Exception {
+        assertNotEquals(new Fraction(1,5), new Fraction(2,5));
+    }
+
+    @Test
+    public void differentDenominators() throws Exception {
+        assertNotEquals(new Fraction(3,4), new Fraction(3,7));
+    }
+
+    @Test
+    public void wholeNumberEqualsSameFraction() throws Exception {
+        assertEquals(new Fraction(5,1), new Fraction(5));
+    }
+
+    @Test
+    public void wholeNumbersdNotEqualToDifferentWholeNumber() throws Exception {
+        assertNotEquals(new Fraction(6), new Fraction(5));
+    }
+
+/*
+    @Test
+    public void reduce() throws Exception {
+        assertNotEquals(new Fraction(3,4), new Fraction(6,8));
+    }
+*/
+
 
 }
