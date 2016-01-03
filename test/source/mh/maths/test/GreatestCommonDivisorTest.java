2aaba334ef24f4aaf9b179ae2708948ee9d9674f
mat hayes <mat.hayes@outlook.com>
2016-01-03 11:13:54 +0000

    tested GCD - found it comes out negative... dont want this.

--- test/source/mh/maths/test/GreatestCommonDivisorTest.java
+++ test/source/mh/maths/test/GreatestCommonDivisorTest.java
@@ -10,9 +10,31 @@ import static org.junit.Assert.assertEquals;
 public class GreatestCommonDivisorTest {
 
     @Test
-    public void oneAndOne() throws Exception {
+    public void reflexive() throws Exception {
         assertEquals(1, gcd(1,1));
+        assertEquals(2, gcd(2,2));
+        assertEquals(-2, gcd(-2,-2));
     }
+
+    @Test
+    public void relativelyPrime() throws Exception {
+        assertEquals(1, gcd(2,3));
+        assertEquals(1, gcd(4,7));
+        assertEquals(-1, gcd(-4,-7));
+    }
+    @Test
+    public void multipleOfOther() throws Exception {
+        assertEquals(3, gcd(3,9));
+        assertEquals(5, gcd(5,25));
+    }
+
+    @Test
+    public void commonFactor() throws Exception {
+        assertEquals(2, gcd(6,8));
+        assertEquals(7, gcd(49,315));
+        assertEquals(-4, gcd(-24,-28));
+    }
+
 
     private int gcd(int a, int b) {
 
