80635133b67fa6f6f0b8ac8e5b1ac7132d9be318
mat hayes <mat.hayes@outlook.com>
2016-01-03 12:02:53 +0000

    added more tests. missing case for GCD.

--- test/source/mh/maths/test/AddFractionsTest.java
+++ test/source/mh/maths/test/AddFractionsTest.java
@@ -1,6 +1,7 @@
 package mh.maths.test;
 
 import mh.maths.Fraction.Fraction;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.internal.requests.FilterRequest;
 
@@ -53,6 +54,33 @@ public class AddFractionsTest {
     @Test
     public void reduceResultToWholeNumber() throws Exception {
         assertEquals(new Fraction(1), new Fraction(1,3).plus(new Fraction(2,3)));
+    }
+
+    @Test
+    public void oneDenominatorIisMulitpleOfOther() throws Exception {
+        assertEquals(new Fraction(11,8), new Fraction(3,4).plus(new Fraction(5,8)));
+    }
+
+    @Test
+    public void commonFactorInDenominator() throws Exception {
+        assertEquals(new Fraction(11,18), new Fraction(1,6).plus(new Fraction(4,9)));
+    }
+
+    @Test
+    public void reduceResultWhenDenomIsTheSame() throws Exception {
+        assertEquals(new Fraction(3,2), new Fraction(3,4).plus(new Fraction(3,4)));
+    }
+
+    @Test
+    public void negativeFractionAndReducing() throws Exception {
+        assertEquals(new Fraction(1,2), new Fraction(-1,4).plus(new Fraction(3,4)));
+        assertEquals(new Fraction(-1,8), new Fraction(3,8).plus(new Fraction(-1,2)));
+    }
+
+    @Test
+    @Ignore("Constructor doesnt yet avoid neg denom")
+    public void negativeEverywhere() throws Exception {
+        assertEquals(new Fraction(1,2), new Fraction(1,-4).plus(new Fraction(-3,-4)));
     }
 
 
