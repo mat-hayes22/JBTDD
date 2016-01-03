16753936c48282585acd179c531583ddfc992458
mat hayes <mat.hayes@outlook.com>
2016-01-03 10:18:33 +0000

    refactored tests

--- test/source/mh/maths/test/AddFractionsTest.java
+++ test/source/mh/maths/test/AddFractionsTest.java
@@ -12,31 +12,30 @@ public class AddFractionsTest {
 
     @Test
     public void zeroPlusZero() throws Exception {
-        assertEquals(0, new Fraction(0).plus(new Fraction(0)).intValue());
+        assertEquals(new Fraction(0), new Fraction(0).plus(new Fraction(0)));
 
     }
 
     @Test
     public void nonZeroPlusZero() throws Exception {
-        assertEquals(3, new Fraction(3).plus(new Fraction(0)).intValue());
+        assertEquals(new Fraction(3), new Fraction(3).plus(new Fraction(0)));
 
     }
 
     @Test
     public void zeroPlusNonZero() throws Exception {
-        assertEquals(5, new Fraction(0).plus(new Fraction(5)).intValue());
+        assertEquals(new Fraction(5), new Fraction(0).plus(new Fraction(5)));
 
     }
 
     @Test
     public void nonNegativeNonZeroOperand() throws Exception {
-        assertEquals(7, new Fraction(3).plus(new Fraction(4)).intValue());
-
+        assertEquals(new Fraction(7), new Fraction(3).plus(new Fraction(4)));
     }
 
     @Test
     public void negativeInputsAndNegativeOutput() throws Exception {
-        assertEquals(-2, new Fraction(-3).plus(new Fraction(1)).intValue());
+        assertEquals(new Fraction(-2), new Fraction(-3).plus(new Fraction(1)));
 
     }
 
