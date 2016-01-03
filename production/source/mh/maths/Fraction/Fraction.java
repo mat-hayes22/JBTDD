4c6923ecd0744171ae06c09fb5c705901817b1c7
mat hayes <mat.hayes@outlook.com>
2016-01-03 10:19:48 +0000

    removed obselete code

--- production/source/mh/maths/Fraction/Fraction.java
+++ production/source/mh/maths/Fraction/Fraction.java
@@ -23,18 +23,6 @@ public class Fraction {
             return new Fraction(this.numerator + that.numerator, denominator );
     }
 
-    public int intValue() {
-        return numerator;
-    }
-
-    public int getNumerator() {
-        return numerator;
-    }
-
-    public int getDenominator() {
-        return denominator;
-    }
-
     @Override
     public boolean equals(Object other) {
 
